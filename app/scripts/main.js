f359b21a4b02efca9912d59e17d5d592cb9c7614
Isac Fadoni <isacfadoni@outlook.com>
2015-10-03 23:34:09 -0300

    Dialogo de informações criado

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+// Script for UI Interaction and Behavior
+
+
+'use strict';
+
+/* 
+ * Open/Close a dialog with some information to user.
+ */
+var INFO_DIALOG = document.querySelector('#modalone');
+var INFO_BUTTON = document.querySelector('.material-button[href="#comofunciona"]');
+var EXIT_BUTTON = document.querySelector('.material-dialog footer a[href="#fechar"]');
+
+var SHOW_DIALOG = function() {
+  INFO_DIALOG.classList.add('show');
+  INFO_BUTTON.removeEventListener('click', SHOW_DIALOG, false);
+};
+
+var EXIT_DIALOG = function() {
+  INFO_DIALOG.classList.remove('show');
+  INFO_BUTTON.addEventListener('click', SHOW_DIALOG, false);
+};
+
+EXIT_BUTTON.addEventListener('click', EXIT_DIALOG, false);
+INFO_BUTTON.addEventListener('click', SHOW_DIALOG, false);
